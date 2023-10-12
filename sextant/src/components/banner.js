201cc19ce21228e28919c47c68a5332efa1a5269
Sergio <s.anguloi@outlook.com>
2023-10-12 18:54:22 -0400

    Removed unused files and added header and exhibit

---
+++ sextant/src/components/banner.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './banner.module.css'
+
+
+const Banner = ({ title }) => {
+  return (
+    <div className={styles.banner}>
+      <h1>Header1</h1>
+    </div>
+  )
+}
+
+export default Banner
---
+++ sextant/src/components/exhibit.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from './exhibit.module.css'
+
+const Exhibit = ({ header, children }) => {
+  return (
+    <div className={styles.card}>
+      <div className={styles.header}>{header}</div>
+      <br />
+      {children}
+    </div>
+  )
+}
+
+export default Exhibit
